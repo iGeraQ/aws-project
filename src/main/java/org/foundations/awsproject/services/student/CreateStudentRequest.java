905d2b1075d6f3436915be4c613959c0270817a5
iGeraQ <a22216369@alumnos.uady.mx>
2026-03-30 19:53:54 -0600

    fix: student gpa entry data validation

--- src/main/java/org/foundations/awsproject/services/student/CreateStudentRequest.java
+++ src/main/java/org/foundations/awsproject/services/student/CreateStudentRequest.java
@@ -1,10 +1,13 @@
 package org.foundations.awsproject.services.student;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record CreateStudentRequest(
         @NotBlank String name,
         @NotBlank String lastname,
         @NotBlank String studentId,
-        @NotBlank float gpa
+        @NotNull @Positive @Max(4) float gpa
 ) {
 }
